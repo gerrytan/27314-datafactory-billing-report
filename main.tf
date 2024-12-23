bbcb497c631b87f410b4dc5d4d1b0c527d835ad2
Gerry Tan <gerry.tan@microsoft.com>
2024-12-23 15:51:04 +1100

    Managed billing config using azapi

--- main.tf
+++ main.tf
@@ -1,5 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.11.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">=2.1.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+  # export ARM_SUBSCRIPTION_ID="xxxx-xxxx-xxxx-xxxx" to avoid hardcoding the subscription_id
+}
+
+provider "azapi" {
   # export ARM_SUBSCRIPTION_ID="xxxx-xxxx-xxxx-xxxx" to avoid hardcoding the subscription_id
 }
 
@@ -16,15 +33,25 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_data_factory" "adf" {
-  name                = "${var.prefix}-adf"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# Commented out, replaced with azapi_resource.adf so billing config can be managed
+# resource "azurerm_data_factory" "adf" {
+#   name                = "${var.prefix}-adf"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
 
-  # Toggle this parameter to force update resource
-  global_parameter {
-    name  = "intVal"
-    type  = "Int"
-    value = "3"
+resource "azapi_resource" "adf" {
+  type                      = "Microsoft.DataFactory/factories@2018-06-01"
+  name                      = "${var.prefix}-adf"
+  location                  = azurerm_resource_group.rg.location
+  parent_id                 = azurerm_resource_group.rg.id
+  schema_validation_enabled = false
+
+  body = {
+    properties = {
+      globalConfigurations = {
+        PipelineBillingEnabled = "true"
+      }
+    }
   }
 }
